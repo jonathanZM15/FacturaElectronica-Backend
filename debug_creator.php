111eb86053cc3a87732fbb92570cf45baf45828a
jonathanZM15 <jonathanz1504@gmail.com>
2026-02-05 15:20:42 -0500

    Eliminar archivos de depuración y scripts no utilizados

--- debug_creator.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$user = App\Models\User::find(1);
-
-echo "Usuario ID 1:\n";
-print_r($user->toArray());
